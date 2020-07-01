ff4be7097680ed58ea51169c4561ec28bfce6374
Gregory Anne <ganne@effisoft-group.com>
2020-07-01 18:23:52 +0200

    update of prettier plugin

--- src/main/java/com/github/zg2pro/formatter/plugin/util/DependenciesVersions.java
+++ src/main/java/com/github/zg2pro/formatter/plugin/util/DependenciesVersions.java
@@ -28,5 +28,5 @@ package com.github.zg2pro.formatter.plugin.util;
  * @author zg2pro
  */
 public class DependenciesVersions {
-    public static String PRETTIER_JAVA_MAVEN_PLUGIN_VERSION = "0.8";
+    public static String PRETTIER_JAVA_MAVEN_PLUGIN_VERSION = "0.9";
 }
