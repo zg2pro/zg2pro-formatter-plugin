648dffb38df44997473145b6a16003be24cdf724
Anne, Gregory <gregory.anne@duckcreek.com>
2024-06-26 13:55:44 +0200

    git hook as an option

--- src/main/java/com/github/zg2pro/formatter/plugin/util/DependenciesVersions.java
+++ src/main/java/com/github/zg2pro/formatter/plugin/util/DependenciesVersions.java
@@ -29,6 +29,5 @@ package com.github.zg2pro.formatter.plugin.util;
  */
 public class DependenciesVersions {
     public static String SPOTLESS_SCALA_MAVEN_PLUGIN_VERSION = "2.17.0";
-    public static String PRETTIER_JAVA_MAVEN_PLUGIN_VERSION = "0.14";
-    public static String NODE_VERSION = "14.18.0";
+    public static String PRETTIER_JAVA_MAVEN_PLUGIN_VERSION = "0.16";
 }
